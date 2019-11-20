0adb07b2cefd29c3d793d9b6ed1a5e8dedfe6c47
Lucifer <ars9979@icloud.com>
2019-11-20 14:54:04 +0100

    Link and functionality of link added
    Link will be used to create linked list

---
+++ Algorithms/src/LinkedList/Link.java
@@ -0,0 +1,49 @@
+package LinkedList;
+
+public class Link {
+    private String name;
+    private int value;
+
+    private Link next;
+
+    public Link(String name, int value) {
+        this.name = name;
+        this.value = value;
+    }
+
+    public void display(){
+        System.out.println(toString());
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    public Link getNext() {
+        return next;
+    }
+
+    public void setNext(Link next) {
+        this.next = next;
+    }
+
+    @Override
+    public String toString() {
+        return "Link{" +
+                "name='" + name + '\'' +
+                ", value=" + value +
+                '}';
+    }
+}
