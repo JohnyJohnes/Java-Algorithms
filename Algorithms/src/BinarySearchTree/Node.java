d9a23a9ca6153fd1d1b8d947c0ee9fcd5c1c1c3a
Lucifer <ars9979@icloud.com>
2019-12-03 15:53:35 +0100

    Node class implementation added

--- Algorithms/src/BinarySearchTree/Node.java
+++ Algorithms/src/BinarySearchTree/Node.java
@@ -1,4 +1,22 @@
 package BinarySearchTree;
 
 public class Node {
+    int key;
+    String name;
+
+    Node leftChild;
+    Node rightChild;
+
+    public Node(int key, String name) {
+        this.key = key;
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Node{" +
+                "key=" + key +
+                ", name='" + name + '\'' +
+                '}';
+    }
 }
