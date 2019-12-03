5bdf751ea1ff0c1c0f9eaff7b106182c73bad7b3
Lucifer <ars9979@icloud.com>
2019-12-03 16:10:04 +0100

    postOrderTraverseTree method implemented into BinaryTree class

--- Algorithms/src/BinarySearchTree/BinaryTree.java
+++ Algorithms/src/BinarySearchTree/BinaryTree.java
@@ -58,5 +58,16 @@ public class BinaryTree {
         }
     }
 
+    public void postOrderTraverseTree(Node focusNode){
+        if (focusNode!=null){
+
+            postOrderTraverseTree(focusNode.leftChild);
+
+            postOrderTraverseTree(focusNode.rightChild);
+
+            System.out.println(focusNode);
+        }
+    }
+
     public static void main(String[] args){}
 }
