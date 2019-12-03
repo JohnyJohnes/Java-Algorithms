93241553e02f92670898c691aa3a705de7d0fcda
Lucifer <ars9979@icloud.com>
2019-12-03 16:17:49 +0100

    main function implemented to call and test other methods of BinaryTree class

--- Algorithms/src/BinarySearchTree/BinaryTree.java
+++ Algorithms/src/BinarySearchTree/BinaryTree.java
@@ -84,5 +84,37 @@ public class BinaryTree {
         }
     }
 
-    public static void main(String[] args){}
+    public static void main(String[] args){
+        BinaryTree theTree = new BinaryTree();
+
+        theTree.addNode(50, "Boss");
+
+        theTree.addNode(25, "Vice President");
+
+        theTree.addNode(15, "Office Manager");
+
+        theTree.addNode(30, "Secretary");
+
+        theTree.addNode(75, "Sales Manager");
+
+        theTree.addNode(85, "Salesman 1");
+
+        System.out.println();
+
+         theTree.inOrderTraverseTree(theTree.root);
+
+        System.out.println();
+
+         theTree.preOrderTraverseTree(theTree.root);
+
+        System.out.println();
+
+         theTree.postOrderTraverseTree(theTree.root);
+
+        System.out.println();
+
+        System.out.println("\nNode with the key 75");
+
+        System.out.println(theTree.findNode(75));
+    }
 }
